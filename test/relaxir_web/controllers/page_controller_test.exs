ab8a693ea2d8633a2abd3ff85bc330d047efad56
Faheetah <Faheetah@users.noreply.github.com>
2020-08-12 00:42:38 -0500

    Remove unused tests

--- test/relaxir_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule RelaxirWeb.PageControllerTest do
-  use RelaxirWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  end
-end
--- test/relaxir_web/views/page_view_test.exs
+++
@@ -1,3 +0,0 @@
-defmodule RelaxirWeb.PageViewTest do
-  use RelaxirWeb.ConnCase, async: true
-end
