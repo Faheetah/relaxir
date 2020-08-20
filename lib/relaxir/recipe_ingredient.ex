088701a214bbd81affb9147dcc7fc4fc7c46f2a5
Faheetah <Faheetah@users.noreply.github.com>
2020-08-20 13:15:18 -0500

    Add amount field

--- lib/relaxir/recipe_ingredient.ex
+++ lib/relaxir/recipe_ingredient.ex
@@ -5,13 +5,14 @@ defmodule Relaxir.RecipeIngredient do
   alias Relaxir.Ingredients.Ingredient
 
   schema "recipe_ingredients" do
+    field :amount, :integer
     belongs_to :recipe, Recipe, foreign_key: :recipe_id
     belongs_to :ingredient, Ingredient, foreign_key: :ingredient_id
   end
 
   def changeset(recipe_ingredient, attrs) do
     recipe_ingredient
-    |> cast(attrs, [:recipe_id, :ingredient_id])
+    |> cast(attrs, [:recipe_id, :ingredient_id, :amount])
     |> cast_assoc(:recipe)
     |> cast_assoc(:ingredient)
   end
---
+++ priv/repo/migrations/20200820181401_modify_recipes_ingredients_add_amount.exs
@@ -0,0 +1,9 @@
+defmodule Relaxir.Repo.Migrations.ModifyRecipesIngredientsAddAmount do
+  use Ecto.Migration
+
+  def change do
+    alter table(:recipe_ingredients) do
+      add :amount, :integer
+    end
+  end
+end
