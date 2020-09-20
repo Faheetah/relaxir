d716fe045a4a64ee7b2410855cf181c068b9c843
Faheetah <Faheetah@users.noreply.github.com>
2020-09-20 05:22:43 -0500

    Use explicit search terms instead of fuzzy which should have O(n) time

--- lib/relaxir/search/search.ex
+++ lib/relaxir/search/search.ex
@@ -9,9 +9,21 @@ defmodule Relaxir.Search do
     ], opts)
   end
 
+  def tomatoes() do
+    food("green tomatoes")
+  end
+
+  def food(name) do
+    # {"Tutturosso Green 14.5oz. Italian Diced Tomatoes", 3.0},
+    :timer.tc fn ->
+      Relaxir.Search.get(Relaxir.Ingredients.Food, :description, name)
+    end
+  end
+
+  # Relaxir.Search.get(Relaxir.Ingredients.Food, :description, "green tomatoes")
   def get(module, name, item) do
     table = atom_from_module(module, name)
-    case GenServer.call(__MODULE__, {:get, table, item}, 20000) do
+    case GenServer.call(__MODULE__, {:get, table, item}, 5000) do
       [] -> {:error, :not_found}
       results -> {:ok, results}
     end
@@ -23,24 +35,67 @@ defmodule Relaxir.Search do
   end
 
   defp compare(left, right) do
+    # cond do
+      # left == right -> 1.0
+      # true -> String.jaro_distance(left, right)
+    # end
     cond do
-      left == right -> {1.0, left}
-      String.contains?(left, right) -> {0.9 + (0.1 / abs(String.length(left) - String.length(right))), left}
-      true -> {String.jaro_distance(left, right), left}
+      left == right -> 1.0
+      true -> 0.0
     end
   end
 
-  def handle_call({:get, table, item}, _from, state) do
+  defp compare_multiple(left, rights) do
+    rights
+    |> Enum.map(&({compare(&1, left), left}))
+    |> Enum.sort_by(fn {i, _} -> i end, :desc)
+    |> hd
+  end
+
+  # this search takes an absurdly long time the longer the search terms are
+  # it does not scale well at all, but could be useful for correlating possible similar ingredients
+  def handle_call({:get_fuzzy, table, item}, _from, state) do
+    items = Regex.scan(~r/[a-zA-Z]+/, item)
+    |> Enum.map(fn [i] -> i end)
+    |> Enum.reject(&(&1 == "" || &1 == nil))
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(&String.downcase/1)
+    |> Enum.reject(&(&1 == "" || &1 == nil))
+
+    prefixes = Enum.map(items, &String.first/1)
+
     results =
-      :ets.match(table, {:"$1"})
-      |> Stream.flat_map(fn i -> i end)
-      |> Stream.reject(&(&1 == nil))
-      |> Stream.map(&String.downcase/1)
-      |> Stream.map(&compare(&1, item))
-      |> Stream.reject(fn {i, _} -> i < 0.7 end)
-      |> Stream.take(100)
-      |> Enum.sort_by(fn {i, _} -> i end, :desc)
-      |> Enum.take(5)
+      # :ets.fun2ms(fn {"letter", word, full} -> {word, full} end)
+      # Where "letter" is being passed in as k
+      :ets.select(table, (for k <- prefixes, do: {{k, :"$1", :"$2"}, [], [{{:"$1", :"$2"}}]}))
+      |> Stream.map(fn {k, v} -> {compare_multiple(k, items), v} end)
+      |> Stream.reject(fn {{i, _}, _} -> i < 0.85 end)
+      |> Enum.reduce(%{}, fn(i, acc) ->
+        {{score, _}, name} = i
+        Map.update(acc, name, 1, &(&1 + score + (100 / String.length(name))))
+      end)
+      |> Enum.sort_by(fn {_, score} -> score end, :desc)
+      |> Enum.take(20)
+
+    {:reply, results, state}
+  end
+
+  def handle_call({:get, table, item}, _from, state) do
+    items = Regex.scan(~r/[a-zA-Z]+/, item)
+    |> Enum.map(fn [i] -> i end)
+    |> Enum.reject(&(&1 == "" || &1 == nil))
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(&String.downcase/1)
+    |> Enum.reject(&(&1 == "" || &1 == nil))
+
+    results =
+      # :ets.fun2ms(fn {keyword, full} -> {full} end)
+      :ets.select(table, (for i <- items, do: {{i, :"$1"}, [], [{{:"$1"}}]}))
+      |> Enum.reduce(%{}, fn({name}, acc) ->
+        Map.update(acc, name, 1, &(&1 + (100 / String.length(name))))
+      end)
+      |> Enum.sort_by(fn {_, score} -> score end, :desc)
+      |> Enum.take(20)
 
     {:reply, results, state}
   end
@@ -56,12 +111,28 @@ defmodule Relaxir.Search do
     Enum.each(tables, fn {table, fields} ->
       Enum.each(fields, fn field ->
         table_name = String.to_atom("#{table}_#{field}")
-        :ets.new(table_name, [:named_table, :set, :private])
+        :ets.new(table_name, [:named_table, :duplicate_bag]) #:private
 
-        module = module_from_atom(table)
+        module_from_atom(table)
+        |> repo.all
+        |> Enum.map(fn i ->
+          Map.get(i, field)
+        end)
+        |> Enum.sort
+        |> Stream.dedup
+        |> Enum.each(fn value ->
 
-        repo.all(module)
-        |> Enum.each(fn i -> :ets.insert(table_name, {Map.get(i, field)}) end)
+          unless value == nil || value == "" do
+            Regex.scan(~r/[a-zA-Z]+/, value)
+            |> Enum.map(fn [i] -> i end)
+            |> Enum.reject(&(&1 == "" || &1 == nil))
+            |> Enum.sort
+            |> Enum.dedup
+            |> Enum.each(fn v ->
+              :ets.insert(table_name, {String.downcase(v), value})
+            end)
+          end
+        end)
       end)
     end)
 
@@ -69,6 +140,7 @@ defmodule Relaxir.Search do
 
   end
 
+  # table_name -> Elixir.Table.Name
   defp module_from_atom(table) do
     table
     |> Atom.to_string
@@ -79,6 +151,7 @@ defmodule Relaxir.Search do
     |> String.to_existing_atom
   end
 
+  # Elixir.Table.Name -> table_name
   defp atom_from_module(module, name) do
     module
     |> Atom.to_string
