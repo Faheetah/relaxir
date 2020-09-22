a987255f4065c7d0ef237f0cdeb2038bfa30b616
Faheetah <Faheetah@users.noreply.github.com>
2020-09-21 21:51:04 -0500

    Remove singular plural columns

---
+++ priv/repo/migrations/20200922024927_modify_units_remove_singular_plural_columns.exs
@@ -0,0 +1,10 @@
+defmodule Relaxir.Repo.Migrations.ModifyUnitsRemoveSingularPluralColumns do
+  use Ecto.Migration
+
+  def change do
+    alter table(:units) do
+      remove :singular, :string
+      remove :plural, :string
+    end
+  end
+end
