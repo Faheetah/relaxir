46852b3ed6f29f4b42eceeeb4e2f338f7d137f4a
Faheetah <Faheetah@users.noreply.github.com>
2020-10-02 15:04:19 -0500

    Bump mix verison

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Relaxir.MixProject do
   def project do
     [
       app: :relaxir,
-      version: "0.4.7",
+      version: "0.4.8",
       url: "https://github.com/Faheetah/relaxir/",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
