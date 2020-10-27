54e91a566d6cf53ac865f297461b75a169f8ebb0
Faheetah <Faheetah@users.noreply.github.com>
2020-10-26 21:01:00 -0500

    Filter search and search from lists

--- lib/relaxir_web/controllers/search_controller.ex
+++ lib/relaxir_web/controllers/search_controller.ex
@@ -1,6 +1,13 @@
 defmodule RelaxirWeb.SearchController do
   use RelaxirWeb, :controller
 
+  @checkboxes %{
+    recipes: true,
+    categories: true,
+    ingredients: true,
+    usda: true
+  }
+
   def search_for(module, name, terms) do
     case Relaxir.Search.get(module, name, terms) do
       {:ok, results} -> results
@@ -9,25 +16,61 @@ defmodule RelaxirWeb.SearchController do
     |> Enum.map(fn {i, _} -> i end)
   end
 
-  def search(conn, %{"terms" => terms}) do
+  # default search page
+  def search(conn, params) when map_size(params) == 0 do
     current_user = RelaxirWeb.Authentication.get_current_user(conn)
-
-    recipes = search_for(Relaxir.Recipes.Recipe, :title, terms)
-    categories = search_for(Relaxir.Categories.Category, :name, terms)
-    ingredients = search_for(Relaxir.Ingredients.Ingredient, :name, terms)
-    food = search_for(Relaxir.Ingredients.Food, :description, terms)
-
-    count = Enum.reduce([recipes, categories, ingredients, food], 0, fn i, acc -> acc + length(i) end)
-    render(conn, "search.html", current_user: current_user, count: count, results: %{
-      "recipes" => Enum.take(recipes, 20),
-      "categories" => Enum.take(categories, 20),
-      "ingredients" => Enum.take(ingredients, 20),
-      "usda" => Enum.take(food, 20)
-    })
+    render(conn, "search.html", current_user: current_user, count: :na, checkboxes: @checkboxes, results: [])
   end
 
-  def search(conn, _params) do
+  # search results
+  def search(conn, params) do
+    IO.inspect params
+    terms = params["terms"]
     current_user = RelaxirWeb.Authentication.get_current_user(conn)
-    render(conn, "search.html", current_user: current_user, count: :na, results: [])
+
+    checkboxes = %{
+      recipes: Map.get(params, "recipes", false) == "true",
+      categories: Map.get(params, "categories", false) == "true",
+      ingredients: Map.get(params, "ingredients", false) == "true",
+      usda: Map.get(params, "usda", false) == "true"
+    }
+
+    recipes =
+      case checkboxes.recipes do
+        true -> search_for(Relaxir.Recipes.Recipe, :title, terms)
+        _ -> []
+      end
+
+    categories =
+      case checkboxes.categories do
+        true -> search_for(Relaxir.Categories.Category, :name, terms)
+        _ -> []
+      end
+
+    ingredients =
+      case checkboxes.ingredients do
+        true -> search_for(Relaxir.Ingredients.Ingredient, :name, terms)
+        _ -> []
+      end
+
+    usda =
+      case checkboxes.usda do
+        true -> search_for(Relaxir.Ingredients.Food, :description, terms)
+        _ -> []
+      end
+
+    count = Enum.reduce([recipes, categories, ingredients, usda], 0, fn i, acc -> acc + length(i) end)
+
+    render(conn, "search.html",
+      current_user: current_user,
+      count: count,
+      checkboxes: checkboxes,
+      results: %{
+        "recipes" => Enum.take(recipes, 20),
+        "categories" => Enum.take(categories, 20),
+        "ingredients" => Enum.take(ingredients, 20),
+        "usda" => Enum.take(usda, 20)
+      }
+    )
   end
 end
