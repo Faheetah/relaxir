f32f719ce4c7d6fa241344824f7ca29d0c4b2614
Faheetah <Faheetah@users.noreply.github.com>
2020-12-19 14:51:09 -0600

    Better errors for auth

--- lib/relaxir_web/authentication/error_handler.ex
+++ lib/relaxir_web/authentication/error_handler.ex
@@ -4,9 +4,9 @@ defmodule RelaxirWeb.Authentication.ErrorHandler do
   @behaviour Guardian.Plug.ErrorHandler
 
   @impl Guardian.Plug.ErrorHandler
-  def auth_error(conn, {_type, _reason}, _opts) do
+  def auth_error(conn, {type, reason}, _opts) do
     conn
-    |> put_flash(:error, "Authentication error.")
+    |> put_flash(:error, "Authentication error. ${type}: ${reason}")
     |> redirect(to: Routes.session_path(conn, :new))
   end
 end
