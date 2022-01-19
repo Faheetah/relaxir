2be252b2ed52ba6733df9afad95d7298638598c0
Faheetah <Faheetah@users.noreply.github.com>
2022-01-19 13:08:45 -0600

    I guess fix ingredient importer?

--- lib/relaxir/ingredients/ingredient_importer.ex
+++ lib/relaxir/ingredients/ingredient_importer.ex
@@ -18,21 +18,29 @@ defmodule Relaxir.IngredientImporter do
     do_import(path)
     # no clue why rank on Nutrient struggles so much and gets in as a float as a string
     # this coerces that, sadly
-    |> Stream.map(fn %{"rank" => rank} = data ->
-        if rank == "" do
+    |> Stream.map(fn data ->
+        if data["rank"] == "" || data["rank"] == nil do
           data
         else
-          {i, _} = Integer.parse(rank)
+          {i, _} = Integer.parse(data["rank"])
           Map.put(data, "rank", i)
         end
     end)
+    # |> Stream.filter(fn food -> food["data_type"] == nil || food["data_type"] == "foundation_food" || food["data_type"] == "survey_fndds_food" end)
+    |> Stream.filter(fn food -> food["data_type"] == nil || food["data_type"] == "sr_legacy_food" end)
     |> Stream.map(&module.changeset(struct(module, %{}), &1))
     |> Stream.filter(fn entry -> entry.valid? == true end)
     |> Stream.map(&Ecto.Changeset.apply_changes/1)
     |> Stream.map(&Map.take(&1, module.__schema__(:fields)))
     |> Stream.map(fn entry ->
       entry
-      # |> Map.drop([:id])
+      |> then(fn f ->
+        if f.id == nil do
+          Map.drop(f, [:id])
+        else
+          f
+        end
+      end)
       |> Map.merge(%{inserted_at: now, updated_at: now})
     end)
     |> Stream.chunk_every(1000)
