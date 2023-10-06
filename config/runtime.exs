67d50378b542c709fbde1e6e8e0b657be0f9eaa8
Faheetah <Faheetah@users.noreply.github.com>
2023-10-06 11:07:22 -0500

    Use a default image

--- config/runtime.exs
+++ config/runtime.exs
@@ -4,15 +4,17 @@
 # remember to add this file to your .gitignore.
 import Config
 
-config :relaxir, Relaxir.Repo,
-  url: System.fetch_env!("DATABASE_URL"),
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+if config_env() == :prod do
+  config :relaxir, Relaxir.Repo,
+    url: System.fetch_env!("DATABASE_URL"),
+    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
-config :relaxir, RelaxirWeb.Endpoint,
-  http: [
-    port: String.to_integer(System.get_env("PORT") || "4000"),
-    transport_options: [socket_opts: [:inet6]]
-  ],
-  secret_key_base: System.fetch_env!("SECRET_KEY_BASE")
+  config :relaxir, RelaxirWeb.Endpoint,
+    http: [
+      port: String.to_integer(System.get_env("PORT") || "4000"),
+      transport_options: [socket_opts: [:inet6]]
+    ],
+    secret_key_base: System.fetch_env!("SECRET_KEY_BASE")
 
-config :relaxir, RelaxirWeb.Endpoint, server: true
+  config :relaxir, RelaxirWeb.Endpoint, server: true
+end
