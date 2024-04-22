f5b9d3ee6b16bd156d4024e7c392415126504ccd
Faheetah <Faheetah@users.noreply.github.com>
2024-04-22 15:33:58 -0500

    Better design for error pages

--- lib/relaxir_web/views/error_view.ex
+++ lib/relaxir_web/views/error_view.ex
@@ -1,12 +1,6 @@
 defmodule RelaxirWeb.ErrorView do
   use RelaxirWeb, :view
 
-  # If you want to customize a particular status code
-  # for a certain format, you may uncomment below.
-  def render("500.html", _assigns) do
-    "Relax, we are working on it."
-  end
-
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.html" becomes
   # "Not Found".
