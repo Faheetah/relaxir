928b52a8c41fbed817db4cdf6bf0b75512c746eb
William Normand <Faheetah@users.noreply.github.com>
2025-05-30 09:32:40 -0500

    2.0.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Relaxir.MixProject do
   def project do
     [
       app: :relaxir,
-      version: "0.9.0",
+      version: "2.0.3",
       url: "https://github.com/Faheetah/relaxir/",
       elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
