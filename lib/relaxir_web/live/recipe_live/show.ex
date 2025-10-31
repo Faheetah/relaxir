4848919822b0f90bc9027e5d905dceb32f079e42
William Normand <Faheetah@users.noreply.github.com>
2025-10-31 09:43:56 -0500

    Slugify recipe links

--- lib/relaxir_web/live/recipe_live/show.ex
+++ lib/relaxir_web/live/recipe_live/show.ex
@@ -12,10 +12,15 @@ defmodule RelaxirWeb.RecipeLive.Show do
   def handle_params(%{"id" => id}, _, socket) do
     recipe = Recipes.get_recipe!(id)
 
+    slug =
+      recipe.title
+      |> String.downcase
+      |> String.replace(" ", "-")
+
     meta_attrs = %{
       title: recipe.title,
       description: recipe.description,
-      url: Path.join("https://www.relaxanddine.com", ~p"/recipes/#{recipe}"),
+      url: Path.join("https://www.relaxanddine.com", ~p"/recipes/#{recipe.id}/#{slug}"),
       image: get_upload_path(recipe.image_filename)
     }
 
